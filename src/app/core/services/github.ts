48b1a03c1d3e681ad1d6d2c418f8a7535543b044
Mathieu 'Salketer' Parent <salketer@gmail.com>
2025-06-17 18:30:51 +0200

    feat: repositories can now be searched by star and languages

--- src/app/core/services/github.ts
+++ src/app/core/services/github.ts
@@ -1,14 +1,17 @@
 import { inject, Injectable } from '@angular/core';
 import { Octokit } from 'octokit';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { fromPromise } from 'rxjs/internal/observable/innerFrom';
 import { OctokitClient } from '../../providers/Octokit';
+import { ResourceLoader } from '@angular/compiler';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GithubService {
-  octokit = inject(OctokitClient);
+  private http = inject(HttpClient);
+  private octokit = inject(OctokitClient);
   constructor() {}
 
   getCommits() {
@@ -16,10 +19,37 @@ export class GithubService {
     throw new Error('Method not implemented.');
   }
 
-  searchRepositories(term: string) {
+  searchRepositories(search: {
+    term?: string;
+    stars?: number | null;
+    language?: string | null;
+  }) {
+    let query = search.term ?? '';
+    if (search.stars) {
+      query += ` stars:>=${search.stars}`;
+    }
+    if (search.language) {
+      query += ` language:${search.language}`;
+    }
     const req = this.octokit.request('GET /search/repositories', {
-      q: term,
+      q: query,
     });
     return fromPromise(req.then((response) => response.data.items));
+  }
+
+  // Retrieves a list of available programming languages from text file
+  // The file is expected to be in the format: "language_name:hex_color_code"
+  getAvailableLanguages(): Observable<{ name: string; color: string }[]> {
+    return this.http
+      .get('/github-languages.txt', { responseType: 'text' })
+      .pipe(
+        map((data) => {
+          // Assuming the file contains languages separated by new lines
+          return data.split('\n').map((lang) => {
+            const langSplit = lang.split(':');
+            return { name: langSplit[0].trim(), color: langSplit[1] };
+          });
+        })
+      );
   }
 }
--- src/app/pages/repos/repos.ts
+++ src/app/pages/repos/repos.ts
@@ -1,10 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { signal } from '@angular/core';
 import {
   catchError,
   debounceTime,
   distinctUntilChanged,
+  filter,
+  map,
+  startWith,
   switchMap,
   tap,
   throttleTime,
@@ -14,10 +22,15 @@ import { GithubService } from '../../core/services/github';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginator } from '@angular/material/paginator';
-import { DatePipe } from '@angular/common';
+import { AsyncPipe, CommonModule, DatePipe } from '@angular/common';
 import { BasicLayout } from '../../core/layouts/basic/basic';
 import { Avatar } from '../../components/avatar/avatar';
 import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { combineLatest, forkJoin } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-repos',
@@ -29,6 +42,11 @@ import { Router } from '@angular/router';
     DatePipe,
     BasicLayout,
     Avatar,
+    MatIconModule,
+    MatSelectModule,
+    AsyncPipe,
+    CommonModule,
+    MatCardModule,
   ],
   templateUrl: './repos.html',
   styleUrl: './repos.scss',
@@ -39,17 +57,43 @@ export class Repos {
 
   private router = inject(Router);
 
-  protected searchControl = new FormControl('');
-  protected reposDatasource = this.searchControl.valueChanges.pipe(
+  languageFilterCtrl = new FormControl<string | null>('');
+
+  protected readonly filteredLanguages = combineLatest({
+    filterValue: this.languageFilterCtrl.valueChanges.pipe(startWith('')),
+    availableLanguages: this.githubService.getAvailableLanguages(),
+  }).pipe(
     debounceTime(200),
     distinctUntilChanged(),
-    switchMap((term) => {
-      if (!term?.trim()) {
-        return [];
+    map(({ filterValue, availableLanguages }) => {
+      if (!filterValue || filterValue.trim() === '') {
+        return availableLanguages;
       }
+
+      const searchLower = filterValue.toLowerCase();
+      return availableLanguages.filter((language) =>
+        language.name.toLowerCase().includes(searchLower)
+      );
+    })
+  );
+
+  protected readonly searchFormGroup = new FormGroup({
+    term: new FormControl('', { nonNullable: true }),
+    stars: new FormControl(null, Validators.min(0)),
+    language: new FormControl(''),
+  });
+
+  protected searchControl = new FormControl('');
+  protected reposDatasource = this.searchFormGroup.valueChanges.pipe(
+    // Make sure the form is valid before proceeding
+    filter(() => this.searchFormGroup.valid),
+    // Debounce to avoid too many requests
+    debounceTime(200),
+    distinctUntilChanged(),
+    switchMap((searchValue) => {
       this.loading.set(true);
       this.error.set(null);
-      return this.githubService.searchRepositories(term).pipe(
+      return this.githubService.searchRepositories(searchValue).pipe(
         catchError((err) => {
           this.error.set('Failed to fetch repositories');
           this.loading.set(false);
