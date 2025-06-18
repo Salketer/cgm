664da534e714597567c3f11f93adf9c9be1e33c4
Mathieu 'Salketer' Parent <salketer@gmail.com>
2025-06-18 07:23:50 +0200

    feat: Paginator now works correctly with the github API

--- src/app/core/services/github.ts
+++ src/app/core/services/github.ts
@@ -1,11 +1,156 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Octokit } from 'octokit';
-import { map, Observable, of } from 'rxjs';
+import {
+  catchError,
+  map,
+  merge,
+  Observable,
+  of,
+  Subject,
+  Subscription,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { fromPromise } from 'rxjs/internal/observable/innerFrom';
 import { OctokitClient } from '../../providers/Octokit';
 import { ResourceLoader } from '@angular/compiler';
 import { HttpClient } from '@angular/common/http';
+import { DataSource } from '@angular/cdk/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
+export interface RepositoryModel {
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  owner: {
+    name?: string | null;
+    email?: string | null;
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string | null;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+    starred_at?: string;
+    user_view_type?: string;
+  } | null;
+  private: boolean;
+  html_url: string;
+  description: string | null;
+  fork: boolean;
+  url: string;
+  created_at: string;
+  updated_at: string;
+  pushed_at: string;
+  homepage: string | null;
+  size: number;
+  stargazers_count: number;
+  watchers_count: number;
+  language: string | null;
+  forks_count: number;
+  open_issues_count: number;
+  master_branch?: string;
+  default_branch: string;
+  score: number;
+  forks_url: string;
+  keys_url: string;
+  collaborators_url: string;
+  teams_url: string;
+  hooks_url: string;
+  issue_events_url: string;
+  events_url: string;
+  assignees_url: string;
+  branches_url: string;
+  tags_url: string;
+  blobs_url: string;
+  git_tags_url: string;
+  git_refs_url: string;
+  trees_url: string;
+  statuses_url: string;
+  languages_url: string;
+  stargazers_url: string;
+  contributors_url: string;
+  subscribers_url: string;
+  subscription_url: string;
+  commits_url: string;
+  git_commits_url: string;
+  comments_url: string;
+  issue_comment_url: string;
+  contents_url: string;
+  compare_url: string;
+  merges_url: string;
+  archive_url: string;
+  downloads_url: string;
+  issues_url: string;
+  pulls_url: string;
+  milestones_url: string;
+  notifications_url: string;
+  labels_url: string;
+  releases_url: string;
+  deployments_url: string;
+  git_url: string;
+  ssh_url: string;
+  clone_url: string;
+  svn_url: string;
+  forks: number;
+  open_issues: number;
+  watchers: number;
+  topics?: string[];
+  mirror_url: string | null;
+  has_issues: boolean;
+  has_projects: boolean;
+  has_pages: boolean;
+  has_wiki: boolean;
+  has_downloads: boolean;
+  has_discussions?: boolean;
+  archived: boolean;
+  disabled: boolean;
+  visibility?: string;
+  license: {
+    key: string;
+    name: string;
+    url: string | null;
+    spdx_id: string | null;
+    node_id: string;
+    html_url?: string;
+  } | null;
+  permissions?: {
+    admin: boolean;
+    maintain?: boolean;
+    push: boolean;
+    triage?: boolean;
+    pull: boolean;
+  };
+  text_matches?: {
+    object_url?: string;
+    object_type?: string | null;
+    property?: string;
+    fragment?: string;
+    matches?: { text?: string; indices?: number[] }[];
+  }[];
+  temp_clone_token?: string;
+  allow_merge_commit?: boolean;
+  allow_squash_merge?: boolean;
+  allow_rebase_merge?: boolean;
+  allow_auto_merge?: boolean;
+  delete_branch_on_merge?: boolean;
+  allow_forking?: boolean;
+  is_template?: boolean;
+  web_commit_signoff_required?: boolean;
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -19,11 +164,15 @@ export class GithubService {
     throw new Error('Method not implemented.');
   }
 
-  searchRepositories(search: {
-    term?: string;
-    stars?: number | null;
-    language?: string | null;
-  }) {
+  searchRepositories(
+    search: {
+      term?: string;
+      stars?: number | null;
+      language?: string | null;
+    },
+    page: number = 1,
+    perPage: number = 10
+  ): Observable<GitHubResults<RepositoryModel>> {
     let query = search.term ?? '';
     if (search.stars) {
       query += ` stars:>=${search.stars}`;
@@ -33,8 +182,10 @@ export class GithubService {
     }
     const req = this.octokit.request('GET /search/repositories', {
       q: query,
+      page: page,
+      per_page: perPage,
     });
-    return fromPromise(req.then((response) => response.data.items));
+    return fromPromise(req.then((response) => response.data));
   }
 
   // Retrieves a list of available programming languages from text file
@@ -52,4 +203,164 @@ export class GithubService {
         })
       );
   }
+}
+
+export abstract class GitHubDataSource<
+  T extends Record<string, any> = RepositoryModel,
+  P extends MatPaginator = MatPaginator
+> extends DataSource<T> {
+  /**
+   * Instance of the paginator component used by the table to control what page of the data is
+   * displayed. Page changes emitted by the paginator will trigger an update to the
+   * table's rendered data.
+   */
+  get paginator(): P | null {
+    return this._paginator;
+  }
+
+  set paginator(paginator: P | null) {
+    this._paginator = paginator;
+    this._updateChangeSubscription();
+  }
+
+  private _paginator: P | null = null;
+
+  // Emits whenever the page index changes, allowing the data source to update its data.
+  // This is necessary because the paginator only emits after user-generated changes, and we need
+  // to know when to re-render the data if the search criteria reduce the number of items available
+  // to display.
+  private _internalPageChanges = new Subject<void>();
+
+  readonly loading = signal<boolean>(false);
+  readonly error = signal<boolean>(false);
+  /**
+   * Updates the paginator to reflect the length of the filtered data, and makes sure that the page
+   * index does not exceed the paginator's last page. Values are changed in a resolved promise to
+   * guard against making property changes within a round of change detection.
+   */
+  private _updatePaginator(filteredDataLength: number): void {
+    Promise.resolve().then(() => {
+      const paginator = this.paginator;
+      console.log('paginator', paginator);
+      console.log('filteredDataLength', filteredDataLength);
+      if (!paginator) {
+        return;
+      }
+
+      // The GitHub API can only read the first 1000 results, so we limit the paginator length to 1000.
+      paginator.length = Math.min(filteredDataLength, 1000);
+
+      // If the page index is set beyond the page, reduce it to the last page.
+      if (paginator.pageIndex > 0) {
+        const lastPageIndex =
+          Math.ceil(paginator.length / paginator.pageSize) - 1 || 0;
+        const newPageIndex = Math.min(paginator.pageIndex, lastPageIndex);
+
+        if (newPageIndex !== paginator.pageIndex) {
+          paginator.pageIndex = newPageIndex;
+
+          // Since the paginator only emits after user-generated changes,
+          // we need our own stream so we know to should re-render the data.
+          this._internalPageChanges.next();
+        }
+      }
+    });
+  }
+
+  private search: {
+    term?: string;
+    stars?: number | null;
+    language?: string | null;
+  } = {};
+
+  setSearch(search: {
+    term?: string;
+    stars?: number | null;
+    language?: string | null;
+  }) {
+    this.search = search;
+    this._updateChangeSubscription();
+  }
+
+  private _data$$ = new Subject<T[]>();
+
+  connect() {
+    return this._data$$.asObservable();
+  }
+
+  disconnect() {
+    this._renderChangesSubscription?.unsubscribe();
+  }
+
+  private _renderChangesSubscription: Subscription | null = null;
+  private _updateChangeSubscription() {
+    const pageChange: Observable<PageEvent | null | void> = this._paginator
+      ? (merge(
+          this._paginator.page,
+          this._internalPageChanges,
+          this._paginator.initialized
+        ) as Observable<PageEvent | void>)
+      : of(null);
+    // Watched for paged data changes and send the result to the table to render.
+    this._renderChangesSubscription?.unsubscribe();
+    this._renderChangesSubscription = pageChange
+      .pipe(
+        tap(() => {
+          this.loading.set(true);
+          this.error.set(false);
+        }),
+        switchMap((pageChange) => {
+          const page = pageChange ? pageChange.pageIndex + 1 : 1;
+          const perPage = this._paginator ? this._paginator.pageSize : 10;
+          return this._getData(this.search, page, perPage);
+        }),
+        tap((data) => {
+          console.log('data', data);
+          // Update the paginator with the new length of the data.
+          this._updatePaginator(data.total_count);
+        }),
+        map((data) => data.items),
+        tap(() => {
+          this.loading.set(false);
+        }),
+        catchError((error) => {
+          console.debug('Error fetching data:', error);
+          this.loading.set(false);
+          this.error.set(true);
+          return of([]); // Return an empty array on error
+        })
+      )
+      .subscribe((data) => this._data$$.next(data));
+  }
+
+  protected abstract _getData(
+    search: {
+      term?: string;
+      stars?: number | null;
+      language?: string | null;
+    },
+    page: number,
+    perPage: number
+  ): Observable<GitHubResults<T>>;
+}
+
+export class GitHubRepositoriesDataSource extends GitHubDataSource<RepositoryModel> {
+  private githubService = inject(GithubService);
+  protected _getData(
+    search: {
+      term?: string;
+      stars?: number | null;
+      language?: string | null;
+    },
+    page: number = 1,
+    perPage: number = 10
+  ): Observable<GitHubResults<RepositoryModel>> {
+    return this.githubService.searchRepositories(search, page, perPage);
+  }
+}
+
+export interface GitHubResults<T> {
+  total_count: number;
+  incomplete_results?: boolean;
+  items: T[];
 }
--- src/app/pages/repos/repos.ts
+++ src/app/pages/repos/repos.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -7,30 +12,35 @@ import {
 } from '@angular/forms';
 import { signal } from '@angular/core';
 import {
-  catchError,
   debounceTime,
   distinctUntilChanged,
   filter,
   map,
   startWith,
-  switchMap,
-  tap,
-  throttleTime,
 } from 'rxjs/operators';
 import { inject } from '@angular/core';
-import { GithubService } from '../../core/services/github';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {
+  GitHubDataSource,
+  GitHubRepositoriesDataSource,
+  GithubService,
+  RepositoryModel,
+} from '../../core/services/github';
+import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginator } from '@angular/material/paginator';
-import { AsyncPipe, CommonModule, DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { BasicLayout } from '../../core/layouts/basic/basic';
 import { Avatar } from '../../components/avatar/avatar';
 import { Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { combineLatest, forkJoin } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { combineLatest } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
+import {
+  MatProgressSpinner,
+  MatSpinner,
+} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-repos',
@@ -44,9 +54,9 @@ import { MatCardModule } from '@angular/material/card';
     Avatar,
     MatIconModule,
     MatSelectModule,
-    AsyncPipe,
     CommonModule,
     MatCardModule,
+    MatProgressSpinner,
   ],
   templateUrl: './repos.html',
   styleUrl: './repos.scss',
@@ -84,33 +94,34 @@ export class Repos {
   });
 
   protected searchControl = new FormControl('');
-  protected reposDatasource = this.searchFormGroup.valueChanges.pipe(
-    // Make sure the form is valid before proceeding
-    filter(() => this.searchFormGroup.valid),
-    // Debounce to avoid too many requests
-    debounceTime(200),
-    distinctUntilChanged(),
-    switchMap((searchValue) => {
-      this.loading.set(true);
-      this.error.set(null);
-      return this.githubService.searchRepositories(searchValue).pipe(
-        catchError((err) => {
-          this.error.set('Failed to fetch repositories');
-          this.loading.set(false);
-          return [];
-        })
-      );
-    }),
-    tap(() => {
-      this.loading.set(false);
-      new MatTableDataSource<any>([]);
-    })
-  );
+
+  protected reposDatasource = new GitHubRepositoriesDataSource();
+
+  @ViewChild('paginator', { static: true })
+  set paginator(v: MatPaginator | null) {
+    this.reposDatasource.paginator = v;
+  }
+
   protected reposColumns = ['name', 'owner', 'createdAt'];
   loading = signal<boolean>(false);
   error = signal<string | null>(null);
 
   goToCommits(owner: string, repo: string): void {
     this.router.navigate(['/commits', owner, repo]);
+  }
+
+  constructor() {
+    this.searchFormGroup.valueChanges
+      .pipe(
+        // Make sure the form is valid before proceeding
+        filter(() => this.searchFormGroup.valid),
+        // Debounce to avoid too many requests
+        debounceTime(200),
+        distinctUntilChanged(),
+        takeUntilDestroyed()
+      )
+      .subscribe((searchValue) => {
+        this.reposDatasource.setSearch(searchValue);
+      });
   }
 }
